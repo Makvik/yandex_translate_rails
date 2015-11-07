6d66e1e8ee032e72a295f1a275b4f860260fe5e9
Makvik <storm.gman@mail.ru>
2015-11-07 13:30:29 +0300

    many

--- app/controllers/yandex_translates_controller.rb
+++ app/controllers/yandex_translates_controller.rb
@@ -8,32 +8,53 @@ class YandexTranslatesController < ApplicationController
   end
 
   def new
-    yandex = YandexTranslate::Client.new(Key)
-    #exapmle "en-ru" to "English - Russian"
-    @yandex_langs = yandex.get_langs['dirs'].map do |value|
-      split_value = value.split('-')
-      value = Array.[](yandex.get_langs['langs'][split_value[0]] + " - " + yandex.get_langs['langs'][split_value[1]], value)
-    end
     @translate = Translate.new
+    render :action =>'new', :yandex_langs => Yandex_langs
+  end
+
+  def edit
+    @translate = Translate.find(params[:id])
   end
 
   def create
-    yandex = YandexTranslate::Client.new(Key)
     @translate = Translate.new do |t|
       t.text = params.require(:yandex_translates)['text']
-      t.translated_text = yandex.translate(params.require(:yandex_translates)['text'], params.require(:yandex_translates)['lang']).first
+      t.translated_text = Yandex.translate(params.require(:yandex_translates)['text'], params.require(:yandex_translates)['lang']).first
       t.lang_short = params.require(:yandex_translates)['lang']
       split_lang = params.require(:yandex_translates)['lang'].split('-')
-      t.lang_long = yandex.get_langs['langs'][split_lang[0]] + " - " + yandex.get_langs['langs'][split_lang[1]]
+      t.lang_long = Yandex.get_langs['langs'][split_lang[0]] + " - " + Yandex.get_langs['langs'][split_lang[1]]
     end
 
     if @translate.save
       redirect_to :action => "show", :id => @translate
     else
-      render 'new'
+      render :action =>'new', :yandex_langs => Yandex_langs
+    end
+  end
+
+  def update
+    @translate = Translate.find(params[:id]) do |t|
+      t.text = params.require(:yandex_translates)['text']
+      t.translated_text = Yandex.translate(params.require(:yandex_translates)['text'], params.require(:yandex_translates)['lang']).first
+      t.lang_short = params.require(:yandex_translates)['lang']
+      split_lang = params.require(:yandex_translates)['lang'].split('-')
+      t.lang_long = Yandex.get_langs['langs'][split_lang[0]] + " - " + Yandex.get_langs['langs'][split_lang[1]]
+    end
+
+    if @translate.save
+      redirect_to :action => "show", :id => @translate
+    else
+      render 'edit'
     end
   end
 
   private
     Key = "trnsl.1.1.20150807T193721Z.a28754c35f356a3c.9887c0360b34c7c397a5f114144c28840912b234"
+    Yandex = YandexTranslate::Client.new(Key)
+    #exapmle "en-ru" to "English - Russian"
+    Yandex_langs = Yandex.get_langs['dirs'].map do |value|
+      split_value = value.split('-')
+      value = Array.[](Yandex.get_langs['langs'][split_value[0]] + " - " + Yandex.get_langs['langs'][split_value[1]], value)
+    end
+
 end
