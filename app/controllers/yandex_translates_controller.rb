d295835f865897c353f45297bdd4f0e9b8d82c13
Makvik <storm.gman@mail.ru>
2015-11-09 17:09:50 +0300

    moved extra code from the controller not woking new and update basedate

--- app/controllers/yandex_translates_controller.rb
+++ app/controllers/yandex_translates_controller.rb
@@ -8,30 +8,31 @@ class YandexTranslatesController < ApplicationController
   end
 
   def new
-    @translate = Translate.new
-    @yandex_langs = Yandex_langs
+    @translate = Translate.new    
   end
 
   def edit
-    @translate = Translate.find(params[:id])
-    @yandex_langs = Yandex_langs
+    @translate = Translate.find(params[:id])    
   end
 
   def create
-    render_detect_or_translate_or_save('new',
-                      params.require(:commit),
-                      params.require(:yandex_translates)['text'],
-                      params.require(:yandex_translates)['translated_text'],
-                      params.require(:yandex_translates)['lang'])
+    @translate = Translate.new(translate_params)
+
+    if @translate.save
+      redirect_to :action => 'show', :id => @translate
+    else      
+      render 'new'
+    end    
   end
 
   def update
-    render_detect_or_translate_or_save('edit',
-                      params.require(:commit),
-                      params.require(:yandex_translates)['text'],
-                      params.require(:yandex_translates)['translated_text'],
-                      params.require(:yandex_translates)['lang'],
-                      params[:id])
+    @translate = Translate.find(params[:id])
+
+    if @translate.update(translate_params)
+      redirect_to :action => 'show', :id => @translate
+    else      
+      render 'edit'
+    end 
   end
 
   def destroy
@@ -40,61 +41,51 @@ class YandexTranslatesController < ApplicationController
     redirect_to yandex_translates_path
   end
 
-  def render_detect_or_translate_or_save(render_type, commit, text, translated_text, lang, id = 0)
-    # Only treatment button "Detect language"
-    if commit == "Detect language"
-      @yandex_langs = Yandex_langs
-      @translate = Translate.new
-      if render_type == 'edit'
-        @translate = Translate.find(id)
-      end
-      @translate.text = text
-      if !translated_text.empty?
-        @translate.translated_text = translated_text
-      end
-      @translate.lang_short = lang
-      @translate.lang_long = convert_to_lang_long(lang)
-      @detect_lang = Yandex.detect(text)
-      render render_type
-      return
-    end
-
-    # depending on the mode of load or create a resource
-    @translate = Translate.new
-    if render_type == 'edit'
-      @translate = Translate.find(id)
-    end
-    @translate.text = text
-
-    # Save or translate text
-    if commit == "Save" && translated_text.empty?
-      @translate.translated_text = translated_text
-    else
-      @translate.translated_text = Yandex.translate(text, lang).first
-    end
-
-    @translate.lang_short = lang
-    @translate.lang_long = convert_to_lang_long(lang)
-
-    if commit == "Save" && @translate.save
-      redirect_to :action => "show", :id => @translate
-    else
-      @yandex_langs = Yandex_langs
-      render render_type
-    end
-  end
-
-  def convert_to_lang_long(lang)
-    split_lang = lang.split('-')
-    return Yandex.get_langs['langs'][split_lang[0]] + " - " + Yandex.get_langs['langs'][split_lang[1]]
-  end
-
   private
-    Key = "trnsl.1.1.20150807T193721Z.a28754c35f356a3c.9887c0360b34c7c397a5f114144c28840912b234"
-    Yandex = YandexTranslate::Client.new(Key)
-    # "en-ru" to "English - Russian"
-    Yandex_langs = Yandex.get_langs['dirs'].map do |value|
-      split_lang = value.split('-')
-      value = Array.[](Yandex.get_langs['langs'][split_lang[0]] + " - " + Yandex.get_langs['langs'][split_lang[1]], value)
+    def translate_params
+      params.require(:yandex_translates).permit(:text, :translated_text, :lang)
     end
+  # def render_detect_or_translate_or_save(render_type, commit, text, translated_text, lang, id = 0)
+  #   # Only treatment button "Detect language"
+  #   if commit == "Detect language"
+  #     @yandex_langs = Yandex_langs
+  #     @translate = Translate.new
+  #     if render_type == 'edit'
+  #       @translate = Translate.find(id)
+  #     end
+  #     @translate.text = text
+  #     if !translated_text.empty?
+  #       @translate.translated_text = translated_text
+  #     end
+  #     @translate.lang_short = lang
+  #     @translate.lang_long = convert_to_lang_long(lang)
+  #     @detect_lang = Yandex.detect(text)
+  #     render render_type
+  #     return
+  #   end
+
+  #   # depending on the mode of load or create a resource
+  #   @translate = Translate.new
+  #   if render_type == 'edit'
+  #     @translate = Translate.find(id)
+  #   end
+  #   @translate.text = text
+
+  #   # Save or translate text
+  #   if commit == "Save" && translated_text.empty?
+  #     @translate.translated_text = translated_text
+  #   else
+  #     @translate.translated_text = Yandex.translate(text, lang).first
+  #   end
+
+  #   @translate.lang_short = lang
+  #   @translate.lang_long = convert_to_lang_long(lang)
+
+  #   if commit == "Save" && @translate.save
+  #     redirect_to :action => "show", :id => @translate
+  #   else
+  #     @yandex_langs = Yandex_langs
+  #     render render_type
+  #   end
+  # end 
 end
--- app/models/translate.rb
+++ app/models/translate.rb
@@ -3,4 +3,30 @@ class Translate < ActiveRecord::Base
   validates :translated_text, presence: true
   validates :lang_short, presence: true
   validates :lang_long, presence: true
+
+  
+
+  before_validation do 
+    translated_text = Yandex.translate(text, lang).first if translated_text.empty      
+    lang_long = convert_to_lang_long(lang_short)
+  end
+
+  protected
+	  def convert_to_lang_long(lang)
+	    split_lang = lang.split('-')
+	    return Yandex.get_langs['langs'][split_lang[0]] + " - " + Yandex.get_langs['langs'][split_lang[1]]
+	  end
+
+  
+
+  private
+    Key = "trnsl.1.1.20150807T193721Z.a28754c35f356a3c.9887c0360b34c7c397a5f114144c28840912b234"
+    Yandex = YandexTranslate::Client.new(Key)    
+
+  public
+  # "en-ru" to "English - Russian"
+  YANDEX_LANGS = Yandex.get_langs['dirs'].map do |value|
+    split_lang = value.split('-')
+    value = Array.[](Yandex.get_langs['langs'][split_lang[0]] + " - " + Yandex.get_langs['langs'][split_lang[1]], value)
+  end  
 end
