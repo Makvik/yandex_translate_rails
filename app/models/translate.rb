4d1efe2a50b270074fe0c8497659c406dcb28402
Makvik <storm.gman@mail.ru>
2015-11-12 17:09:04 +0300

    go home

--- app/models/translate.rb
+++ app/models/translate.rb
@@ -1,11 +1,15 @@
 class Translate < ActiveRecord::Base
+  private
+    Key = "trnsl.1.1.20150807T193721Z.a28754c35f356a3c.9887c0360b34c7c397a5f114144c28840912b234"
+    yandex = YandexTranslate::Client.new(Key)
+
   validates :text, presence: true
   validates :translated_text, presence: true
   validates :lang, presence: true
 
   before_validation do
-    translated_text = Yandex.translate(text, lang).first if !translated_text.empty
-    # lang_long = convert_to_lang_long(lang_short)
+    translated_text = yandex.translate(text, lang).first
+     # if !translated_text.nil? && translated_text.empty
   end
 
   protected
@@ -16,13 +20,13 @@ class Translate < ActiveRecord::Base
 
 
 
-  private
-    Key = "trnsl.1.1.20150807T193721Z.a28754c35f356a3c.9887c0360b34c7c397a5f114144c28840912b234"
-    yandex = YandexTranslate::Client.new(Key)
+
+
   public
   # "en-ru" to "English - Russian"
-  YANDEX_LANGS = Yandex.get_langs['dirs'].map do |value|
-    split_lang = value.split('-')
-    value = Array.[](Yandex.get_langs['langs'][split_lang[0]] + " - " + Yandex.get_langs['langs'][split_lang[1]], value)
-  end
+  Yandex_langs = yandex.get_langs['dirs']
+  # .map do |value|
+  #   split_lang = value.split('-')
+  #   value = Array.[](Yandex.get_langs['langs'][split_lang[0]] + " - " + Yandex.get_langs['langs'][split_lang[1]], value)
+  # end
 end
