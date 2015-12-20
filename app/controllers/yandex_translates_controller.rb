74bc1a80c63e90f237b5406a55fa1c94c09b6ffa
Makvik <storm.gman@mail.ru>
2015-12-20 23:57:54 +0300

    home work

--- app/controllers/yandex_translates_controller.rb
+++ app/controllers/yandex_translates_controller.rb
@@ -18,11 +18,6 @@ class YandexTranslatesController < ApplicationController
   def create
     @translate = Translate.new(translate_params)
 
-    if params.require(:commit) == "Detect language"
-     detect_lang
-     return
-    end
-
     if @translate.save
       redirect_to :action => 'show', :id => @translate
     else
@@ -46,22 +41,8 @@ class YandexTranslatesController < ApplicationController
     redirect_to yandex_translates_path
   end
 
-  def detect_lang
-    yandex = YandexTranslate::Client.new(Translate::Key)
-    @detect_lang = yandex.detect(params.require(:yandex_translate).permit(:text)).downcase
-
-    render 'edit'
-  end
-
-  def update_lang
-    # @yandex_lang = City.where("country_id = ?", params[:country_id])
-    # respond_to do |format|
-    #   format.js
-    # end
-  end
-
   private
     def translate_params
-      params.require(:yandex_translate).permit(:text, :translated_text, :lang)
+      params.require(:translate).permit(:text)
     end
 end
